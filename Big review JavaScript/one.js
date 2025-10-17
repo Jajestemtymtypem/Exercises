d71e01e1a5bd0f9e621a43204940ba7c3353d6fc
Jajestemtymtypem <jajestemtymtypem@gmail.com>
2025-10-17 08:03:10 +0200

    update

--- Big review JavaScript/one.js
+++ Big review JavaScript/one.js
@@ -1,80 +1,94 @@
-const auto={
-  brand: "Audi",
-  model: "A8",
-  color: "green",
-  years: 2020
-}
-for(const key in auto){
-  console.log(`${key}: ${auto[key]}`)
-}//interuje obiekt wypisuje wartosc za wartoscia 
-
-const tab =[1,2,3,4,5,6,7,8,9,10];
-for(const num of tab){
-  console.log(num);
-}//interuje tablice wypisuje jedna za drugą liczbę z tablicy
-
-Object.entries(auto).forEach(([parametr, wartosc])=> {
-  console.log(`Parametr: ${parametr}: ${wartosc}`)
-})//zmienia obiekt na tablice par klucz watosc i wypisuje ją jedną po drugiej. 
-
-//1 pierwsze nie robie bo już zrobiłem w przykłądzie wyżej szkoda czasu na przepisywanie.
-
-//2
-const players = [
-  { name: "Adam", score: 120 },
-  { name: "Ewa", score: 98 },
-  { name: "Ola", score: 135 }
-];
-// wypisz: "Adam ma 120 pkt." itd.
-
-for(const player of players){
-  console.log(`${player.name} ma ${player.score} pkt.`)
-}
-
-//3
-const product = { name: "Laptop", price: 3500, stock: 12 };
-// wypisz: "name: Laptop | price: 3500 | stock: 12"
-Object.entries(product).forEach(([key, value])=>{
-  console.log(`${key}: ${value}`)
-})
-//4
-const data = {
-  red: [10, 20],
-  blue: [5, 15],
-};
-for (const color in data){
-  const suma = data[color].reduce((acc, curr)=> acc+curr,0)
-  console.log(`${color} -> ${suma}`)
- 
-}
-// 3. 10 minut – quiz logiczny (bez kodu)
-
-// Spróbuj odpowiedzieć z głowy:
-
-// Co wypisze for...in dla tablicy? underfind for in to dla obiektów
-
-// Czy Object.keys() zwraca tablicę? tak tablice kluczy 
-
-// Jak dostać się do wartości price w tablicy obiektów products?tablice rozbił bym przez for of a następnie albo odwołał się do pary albo przez .value albo reduce
-
-// mini projekt powtórkowy
-function analizujUzytkownikow(users) {
-  const tab = Object.entries(users)
-  for(const user of tab){
-    user.reduce((nameUser, scoreUser)=>{
-      if(scoreUser>50){
-        console.log(`Użytkownik:${nameUser} | Punkty:${scoreUser} | Status: OK`)
-      }else{
-         console.log(`Użytkownik:${nameUser} | Punkty:${scoreUser} | Status: LOW`)
-      }
-    })
+//a
+const sklepy = [
+  {
+    nazwa: "Sklep A",
+    produkty: [
+      {nazwa: "Mleko", cena: 3},
+      {nazwa: "Chleb", cena: 2.5}
+    ]
+  },
+  {
+    nazwa: "Sklep B",
+    produkty: [
+      {nazwa: "Masło", cena: 7}
+    ]
+  },
+  {
+    nazwa: "Sklep C",
+    produkty: [
+      {nazwa: "Ser", cena: 5},
+      {nazwa: "Jajka", cena: 6},
+      {nazwa: "Sok", cena: 4.5}
+    ]
   }
-  // użyj Object.entries() i wypisz raport:
-  // "Użytkownik: X | Punkty: Y | Status: OK jeśli Y>=50, LOW jeśli mniej"
+];
+let lacznaWartosc = 0;
+for(const sklep of sklepy){
+  for(const produkt of sklep.produkty){
+    lacznaWartosc += produkt.cena;
+  }
 }
+console.log(lacznaWartosc.toFixed(1));
+//b
+const artykuly = [
+  {tytul: "O psach", tagi: ["zwierzeta", "dom"]},
+  {tytul: "O programowaniu", tagi: ["kod", "javascript"]},
+  {tytul: "O kotach", tagi: ["zwierzeta", "relaks"]},
+  {tytul: "O CSS", tagi: ["kod", "frontend"]}
+];
+for(artykul of artykuly){
+  if(artykul.tagi.includes("kod")){
+    console.log(artykul.tytul)
+  }
+}
+//8
+const rachunki = [
+  {miesiac: "Styczeń", oplaty: {prad: 120, woda: 60}},
+  {miesiac: "Luty", oplaty: {prad: 140, woda: 50}},
+];
+let prad = 0;
+let woda = 0;
+for(const rachunek of rachunki){
+  prad += rachunek.oplaty.prad
+  woda += rachunek.oplaty.woda
+}
+console.log("Łącznie prąd: ",prad)
+console.log("Łącznie woda: ",woda)
 
-analizujUzytkownikow({
-  Ania: 80,
-  Tomek: 45,
-  Basia: 100,
-});
+const suma = rachunki.reduce((acc, rachunek) => {
+  acc.prad += rachunek.oplaty.prad;
+  acc.woda += rachunek.oplaty.woda;
+  return acc;
+}, { prad: 0, woda: 0 }); // początek: wszystko równe 0
+
+console.log(suma)
+
+const dzien = [
+  {godzina: "08:00", zuzycie: {gaz: 1.2, prad: 0.5}},
+  {godzina: "12:00", zuzycie: {gaz: 0.8, prad: 0.9}},
+  {godzina: "18:00", zuzycie: {gaz: 1.0, prad: 0.6}},
+];
+const caleZuzycie = dzien.reduce((acc, godzina)=>{
+  acc.gaz += godzina.zuzycie.gaz;
+  acc.prad += godzina.zuzycie.prad;
+  return acc;
+},{gaz: 0, prad: 0})
+console.log(caleZuzycie)
+//9
+const magazyn = {
+  sekcjaA: [
+    {nazwa: "śrubka", ilosc: 100},
+    {nazwa: "młotek", ilosc: 20}
+  ],
+  sekcjaB: [
+    {nazwa: "wiertło", ilosc: 50}
+  ]
+};
+let calkowitaLiczbaPrzedmiotow = 0;
+let sekcja = Object.entries(magazyn)
+for(const dzial of sekcja){
+  console.log(dzial)
+  for(const produkt of dzial){
+    console.log(produkt)
+  }
+}
