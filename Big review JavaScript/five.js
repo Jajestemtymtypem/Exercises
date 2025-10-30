7870a54c35a49dd507308a1c5a8cfb3043a7c48b
Jajestemtymtypem <jajestemtymtypem@gmail.com>
2025-10-30 07:12:04 +0100

    update

--- Big review JavaScript/five.js
+++ Big review JavaScript/five.js
@@ -65,3 +65,152 @@ function raportSprzedazy(tab){
 console.log(raportSprzedazy(kopiaRaportu))
 
 ///////
+//1
+const oceny = [
+  { imie: "Ania", przedmiot: "matematyka", ocena: 5 },
+  { imie: "Ania", przedmiot: "polski", ocena: 4 },
+  { imie: "Ania", przedmiot: "matematyka", ocena: 3 },
+  { imie: "Kuba", przedmiot: "polski", ocena: 5 },
+  { imie: "Kuba", przedmiot: "matematyka", ocena: 4 }
+];
+function raportOcen(tab){
+  const raport = {};
+  for(const ocena of tab){
+    if(!raport[ocena.imie]){
+      raport[ocena.imie]={};
+    }
+    if(!raport[ocena.imie][ocena.przedmiot]){
+      raport[ocena.imie][ocena.przedmiot] = {
+        suma:0,
+        ilosc:0,
+        srednia:0
+      };
+    }
+    raport[ocena.imie][ocena.przedmiot].ilosc ++;
+    raport[ocena.imie][ocena.przedmiot].suma += ocena.ocena;
+    raport[ocena.imie][ocena.przedmiot].srednia = (raport[ocena.imie][ocena.przedmiot].suma/raport[ocena.imie][ocena.przedmiot].ilosc).toFixed(2);
+  }
+  return raport;
+}
+console.log(raportOcen(oceny));
+
+const projekty = [
+  { pracownik: "Marek", projekt: "A", godziny: 5 },
+  { pracownik: "Marek", projekt: "B", godziny: 3 },
+  { pracownik: "Ewa", projekt: "A", godziny: 4 },
+  { pracownik: "Ewa", projekt: "A", godziny: 2 },
+  { pracownik: "Ola", projekt: "C", godziny: 6 }
+];
+
+function raportGodzin(tab){
+  const raport = {};
+  for(const projekt of tab){
+    if(!raport[projekt.pracownik]){
+      raport[projekt.pracownik]={}  
+    }
+    if(!raport[projekt.pracownik][projekt.projekt]){
+      raport[projekt.pracownik][projekt.projekt]=0;
+    }
+    raport[projekt.pracownik][projekt.projekt] += projekt.godziny
+  }
+  return raport;
+}
+console.log(raportGodzin(projekty))
+
+///3
+const produkty = [
+  { nazwa: "Kawa", kategoria: "napoje", cena: 20 },
+  { nazwa: "Herbata", kategoria: "napoje", cena: 15 },
+  { nazwa: "Mleko", kategoria: "nabiał", cena: 8 },
+  { nazwa: "Ser", kategoria: "nabiał", cena: 12 },
+  { nazwa: "Cukier", kategoria: "słodycze", cena: 5 }
+];
+function raportKategorii(tab){
+const raport = {};
+for(const produkt of tab){
+  if(!raport[produkt.kategoria]){
+    raport[produkt.kategoria] = {
+      liczba:0,
+      lacznaCena:0
+    };
+  }
+  raport[produkt.kategoria].liczba ++;
+  raport[produkt.kategoria].lacznaCena += produkt.cena
+}
+return raport
+}
+console.log(raportKategorii(produkty))
+
+/////
+//1
+const produkty = [
+  { nazwa: "Kawa", cena: 20 },
+  { nazwa: "Mleko", cena: 8 },
+  { nazwa: "Kawa", cena: 22 },
+  { nazwa: "Mleko", cena: 11 },
+  { nazwa: "Cukier", cena: 5 }
+];
+const raport = produkty.reduce((acc, produkt) => {
+  if(!acc[produkt.nazwa]) acc[produkt.nazwa] = { liczba: 0, lacznaCena: 0 };
+  acc[produkt.nazwa].liczba ++;
+  acc[produkt.nazwa].lacznaCena += produkt.cena;
+  return acc;
+},{});
+
+
+console.log(raport)
+
+// function elo(tab){
+//   const raport = {};
+//   for(const produkt of tab){
+//     if(!raport[produkt.nazwa]){
+//       raport[produkt.nazwa] = {
+//         liczba:0,
+//         cena:0
+//       };
+//     }
+//     raport[produkt.nazwa].liczba ++
+//     raport[produkt.nazwa].cena += produkt.cena
+//   }
+//   return raport
+// }
+// console.log(elo(produkty))
+
+// zadanie nr 1 metodę z reduce musiałem przepisać z twojego gotowaca przeanalizowałem zrobiłem błąd szukalem suzkałem aż znalazłem. ale zanim zaczołem robić z reduce to zrobiłem na próbę po staremu tak jak wczoraj jest w komentarzu i to już robiłem sam i działa :D
+//2
+const oceny = [
+  { imie: "Ania", przedmiot: "matematyka", ocena: 5 },
+  { imie: "Ania", przedmiot: "polski", ocena: 4 },
+  { imie: "Kuba", przedmiot: "matematyka", ocena: 3 },
+  { imie: "Kuba", przedmiot: "polski", ocena: 5 }
+];
+
+const raportUczniow = oceny.reduce((acc, ocena) =>{
+  if(!acc[ocena.imie]) acc[ocena.imie]={};
+  acc[ocena.imie][ocena.przedmiot] = ocena.ocena
+  return acc;
+},{});
+console.log(raportUczniow)
+//2 bonus usiadłem do robienie i zastanawiałem się po co upraszaczać ten kod jak on jest taki prosty i gdzie niby miał bym dać drugie reduce i podejrzałem rozwiązanie bez sensu jest teraz je przepisywać . iwidzę że jest to mega trudne do zrozumienia .
+
+
+
+
+// niestety zrobiłem błąd kóry przewidziałeś napisałem zadanie i nie dizałło poprawnie jak przeczytałem twoją podpowiedz okazałą się wpunkt i naprawiło się prawidłowe czytanie i zapisanie danych. 
+//3
+const zamowienia = [
+  { klient: "Ania", kwota: 350 },
+  { klient: "Ania", kwota: 150 },
+  { klient: "Kuba", kwota: 50 },
+  { klient: "Kuba", kwota: 600 },
+  { klient: "Ola", kwota: 100 }
+];
+const raportZamowienia = zamowienia.reduce((acc, zamowienie)=>{
+  if(!acc[zamowienie.klient]) acc[zamowienie.klient] = {suma:0, status:""};
+  acc[zamowienie.klient].suma += zamowienie.kwota;
+  if(acc[zamowienie.klient].suma >= 600) acc[zamowienie.klient].status ="VIP";
+  else if(acc[zamowienie.klient].suma <= 100) acc[zamowienie.klient].status = "LOW"
+  else acc[zamowienie.klient].status = "STANDARD"
+  return acc;
+}, {})
+console.log(raportZamowienia);
--- Big review JavaScript/one.js
+++ Big review JavaScript/one.js
@@ -1,75 +1,85 @@
-//1
-const oceny = [
-  { imie: "Ania", przedmiot: "matematyka", ocena: 5 },
-  { imie: "Ania", przedmiot: "polski", ocena: 4 },
-  { imie: "Ania", przedmiot: "matematyka", ocena: 3 },
-  { imie: "Kuba", przedmiot: "polski", ocena: 5 },
-  { imie: "Kuba", przedmiot: "matematyka", ocena: 4 }
-];
-function raportOcen(tab){
+const slowa = ["kot", "pies", "kot", "mysz", "pies", "kot"];
+function zliczanieSlow(tab){
   const raport = {};
-  for(const ocena of tab){
-    if(!raport[ocena.imie]){
-      raport[ocena.imie]={};
+  for(const slowo of tab){
+    if(!raport[slowo]){
+      raport[slowo] = 0;
     }
-    if(!raport[ocena.imie][ocena.przedmiot]){
-      raport[ocena.imie][ocena.przedmiot] = {
-        suma:0,
-        ilosc:0,
-        srednia:0
+    raport[slowo] ++
+  }
+  return raport
+}
+console.log(zliczanieSlow(slowa))
+const raportSlow = slowa.reduce((acc,slowo)=>{
+  if(!acc[slowo]) acc[slowo] = 0;
+  acc[slowo]++;
+  return acc;
+},{});
+console.log(raportSlow)
+//2
+const gracze = [
+  { nick: "Ania", punkty: 50 },
+  { nick: "Kuba", punkty: 30 },
+  { nick: "Ania", punkty: 20 },
+  { nick: "Ola", punkty: 40 }
+];
+function sumaPkt(tab){
+  const raport = {};
+  for(const gracz of tab){
+    if(!raport[gracz.nick]){
+      raport[gracz.nick] ={
+        sumaPunktow:0
       };
     }
-    raport[ocena.imie][ocena.przedmiot].ilosc ++;
-    raport[ocena.imie][ocena.przedmiot].suma += ocena.ocena;
-    raport[ocena.imie][ocena.przedmiot].srednia = (raport[ocena.imie][ocena.przedmiot].suma/raport[ocena.imie][ocena.przedmiot].ilosc).toFixed(2);
+    raport[gracz.nick].sumaPunktow += gracz.punkty;
   }
   return raport;
 }
-console.log(raportOcen(oceny));
+console.log(sumaPkt(gracze))
 
-const projekty = [
-  { pracownik: "Marek", projekt: "A", godziny: 5 },
-  { pracownik: "Marek", projekt: "B", godziny: 3 },
-  { pracownik: "Ewa", projekt: "A", godziny: 4 },
-  { pracownik: "Ewa", projekt: "A", godziny: 2 },
-  { pracownik: "Ola", projekt: "C", godziny: 6 }
+const raportPkt = gracze.reduce((acc, gracz)=>{
+  if(!acc[gracz.nick]) acc[gracz.nick] ={sumaPunktow:0};
+  acc[gracz.nick].sumaPunktow += gracz.punkty
+  return acc;
+},{});
+console.log(raportPkt)
+//3 musiałem się wpomóc innnym czatem problem sprawiło mi warnek w if i goólnie poruszanie się po tablicy dziwne te zadanie :/
+const uczniowie = [
+  { imie: "Ania", srednia: 5.0 },
+  { imie: "Kuba", srednia: 3.8 },
+  { imie: "Ola", srednia: 2.9 },
+  { imie: "Tomek", srednia: 4.2 }
 ];
-
-function raportGodzin(tab){
-  const raport = {};
-  for(const projekt of tab){
-    if(!raport[projekt.pracownik]){
-      raport[projekt.pracownik]={}  
+function rankingUczniow(tab){
+  const raport = {"Wzorowy":[], "Przeciętny":[], "Słaby":[]};
+  for(const uczen of tab){
+    if(uczen.srednia >4.5) raport["Wzorowy"].push(uczen.imie)
+    else if(uczen.srednia <= 3) raport["Słaby"].push(uczen.imie)
+    else raport["Przeciętny"].push(uczen.imie)
     }
-    if(!raport[projekt.pracownik][projekt.projekt]){
-      raport[projekt.pracownik][projekt.projekt]=0;
-    }
-    raport[projekt.pracownik][projekt.projekt] += projekt.godziny
-  }
   return raport;
 }
-console.log(raportGodzin(projekty))
+console.log(rankingUczniow(uczniowie))
 
-///3
+const srednia = uczniowie.reduce((acc, uczen)=>{
+  if(uczen.srednia >4.5) acc.Wzorowy.push(uczen.imie);
+  else if (uczen.srednia >=3) acc["Przeciętny"].push(uczen.imie);
+  else acc.Slaby.push(uczen.imie)
+  return acc;
+},{Wzorowy:[], "Przeciętny":[], Slaby:[]});
+console.log(srednia)
+//4
 const produkty = [
-  { nazwa: "Kawa", kategoria: "napoje", cena: 20 },
-  { nazwa: "Herbata", kategoria: "napoje", cena: 15 },
-  { nazwa: "Mleko", kategoria: "nabiał", cena: 8 },
-  { nazwa: "Ser", kategoria: "nabiał", cena: 12 },
-  { nazwa: "Cukier", kategoria: "słodycze", cena: 5 }
+  { kategoria: "napoje", cena: 10 },
+  { kategoria: "napoje", cena: 15 },
+  { kategoria: "nabiał", cena: 8 },
+  { kategoria: "nabiał", cena: 12 },
+  { kategoria: "inne", cena: 5 }
 ];
-function raportKategorii(tab){
-const raport = {};
-for(const produkt of tab){
-  if(!raport[produkt.kategoria]){
-    raport[produkt.kategoria] = {
-      liczba:0,
-      lacznaCena:0
-    };
-  }
-  raport[produkt.kategoria].liczba ++;
-  raport[produkt.kategoria].lacznaCena += produkt.cena
-}
-return raport
-}
-console.log(raportKategorii(produkty))
+const raportProduktow = produkty.reduce((acc,produkt)=>{
+  if(!acc[produkt.kategoria]) acc[produkt.kategoria]={liczba:0, suma:0}
+  acc[produkt.kategoria].liczba ++;
+  acc[produkt.kategoria].suma += produkt.cena;
+  return acc;
+},{})
+console.log(raportProduktow)
